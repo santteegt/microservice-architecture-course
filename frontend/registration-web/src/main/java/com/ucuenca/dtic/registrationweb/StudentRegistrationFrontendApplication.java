4bddf05a249c3afefc6a518ce65c42fbc3290fe8
Unknown <renangfz@hotmail.com>
2019-07-15 16:04:04 -0500

    Add registration-web springboot project to frontend submodule

---
+++ frontend/registration-web/src/main/java/com/ucuenca/dtic/registrationweb/StudentRegistrationFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ucuenca.dtic.registrationweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationFrontendApplication.class, args);
+	}
+
+}
---
+++ frontend/registration-web/src/test/java/com/ucuenca/dtic/registrationweb/StudentRegistrationFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ucuenca.dtic.registrationweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentRegistrationFrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
